198559c976adae2cb2e1fcf83b126a85cc02c195
Dawid <dawidadamczykeit@gmail.com>
2020-01-29 23:03:14 +0100

    list, add, edit group functionality

---
+++ src/Controller/GroupOfPeopleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Person;
+use App\Entity\GroupOfPeople;
+use App\Form\GroupType;
+
+class GroupOfPeopleController extends AbstractController
+{
+    public function list()
+    {
+        $repository = $this->getDoctrine()->getRepository(GroupOfPeople::class);
+
+        $groups = $repository->findAll();
+
+        return $this->render('Group/list.html.twig', ['groups' => $groups]);
+    }
+
+    public function new(Request $request)
+    {
+        $group = new GroupOfPeople();
+
+        $form = $this->createForm(GroupType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $group->setName($data['name']);
+            $group->setInfo($data['info']);
+            $group->setCreatedAt(new \DateTime('now'));
+            $group->setUpdatedAt(new \DateTime('now'));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($group);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('Group/new.html.twig', ['form' =>  $form->createView()]);
+    }
+
+    public function edit($id, Request $request)
+    {
+        $group = $this->getDoctrine()
+        ->getRepository(GroupOfPeople::class)
+        ->find($id);
+
+        $form = $this->createForm(GroupType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $group->setName($data['name']);
+            $group->setInfo($data['info']);
+            $group->setCreatedAt(new \DateTime('now'));
+            $group->setUpdatedAt(new \DateTime('now'));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($group);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('Group/new.html.twig', ['form' =>  $form->createView()]);
+    }
+
+    public function delete($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $group = $entityManager
+        ->getRepository(GroupOfPeople::class)
+        ->find($id);
+
+        $entityManager->remove($group);
+        $entityManager->flush();
+
+        return new Response('dupa');
+
+
+    }
+}
--- src/Form/GroupType.php
+++ src/Form/GroupType.php
@@ -6,19 +6,18 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class PersonType extends AbstractType
+
+class GroupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->setMethod('PUT')
-            ->add('login', TextType::class)
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('group', TextType::class)
+            ->add('name', TextType::class)
+            ->add('info', TextareaType::class)
         ;
     }
 
